2efe8421c62c451c8a61a78032cc5709268eb2ea
刘兴宇 <xingyu.liu@dasudian.com>
2019-05-11 15:21:55 +0800

    add

--- gps.sh
+++ gps.sh
@@ -1,10 +1,6 @@
 git add . 
 git commit -m 'add'
-<<<<<<< HEAD
 echo $(git pull origin master) > ~/.tmp_md.txt
-=======
-echo $(git pull origin master) > ~/.tmp.txt
->>>>>>> 2d3dca1482006f5542b4f6d3a1538a6567a62c25
 git push origin master
 
 
