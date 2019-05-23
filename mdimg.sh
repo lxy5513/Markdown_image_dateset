e4cd79fe35be0b1ca9af95ca7f2604a111b63cd5
Ferry <lxy5513@gmail.com>
2019-05-23 14:46:10 +0800

    update

--- mdimg.sh
+++ mdimg.sh
@@ -1,1 +1,2 @@
-./gps.sh > printout && echo '![](https://raw.githubusercontent.com/lxy5513/Markdown_image_dateset/master/'`grep -A -1 -B -1 -oE '[^ ]+$' printout | tail -n 1`')'
+# ./gps.sh > printout && echo '![](https://raw.githubusercontent.com/lxy5513/Markdown_image_dateset/master/'`grep -A -1 -B -1 -oE '[^ ]+$' printout | tail -n 1`')'
+./gps.sh > printout && echo '<p align="center"><img src="https://raw.githubusercontent.com/lxy5513/Markdown_image_dateset/master/'`grep -A -1 -B -1 -oE '[^ ]+$' printout | tail -n 1`'" width="100%" alt="" /></p>'
